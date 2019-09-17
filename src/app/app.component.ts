04b7b169498cdf1b4cb762fcfa059631cbe71044
Stvn_Jvr <estevenjaviier@gmail.com>
2019-09-17 18:41:07 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent { 
 count: number = 0;
 
-   ngOnChanges() {
+   ngAfterContentCheck() {
 
      
     setInterval(() => { this.count = this.count+1; },1000);
