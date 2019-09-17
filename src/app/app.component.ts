ef6989ac6411ddd9acf6c96ca162f7fc5925d676
Stvn_Jvr <estevenjaviier@gmail.com>
2019-09-17 18:44:42 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,15 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent { 
-count: number = 0;
-
-   ngAfterContentChecked() {
-
-     
-    setInterval(() => { this.count = this.count+1; },1000);
-
-    
-    }
-
-}
+export class AppComponent { }
