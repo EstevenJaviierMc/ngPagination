371d8931b8f74cafdc1a864d39964d7476ecf6d6
Stvn_Jvr <estevenjaviier@gmail.com>
2019-09-18 12:21:40 -0500

    Update pagination.component.ts

--- src/app/components/pagination/pagination.component.ts
+++ src/app/components/pagination/pagination.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class PaginationComponent implements OnInit {
 @Input() postsPerPage: number;
 @Input() totalPosts: number;
-currentPage: number;
+currentPage: number = 1;
  pageNumbers = [];
   constructor() { }
 
