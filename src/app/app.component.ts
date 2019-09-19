121f0c8ada9dc9b4163893bdff842af727733a02
Stvn_Jvr <estevenjaviier@gmail.com>
2019-09-19 15:40:26 +0000

    react like

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,35 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent { }
+export class AppComponent {
+  posts: any = [];
+  currentPosts: any = [];
+
+  currentPage: number = 1;
+  postsPerPage: number = 10;
+  indexOfLastPost: number;
+  indexOfFirstPost: number;
+
+  constructor(private http: HttpClient) {
+    // Get current posts
+    this.indexOfLastPost = this.currentPage * this.postsPerPage;
+    this.indexOfFirstPost = this.indexOfLastPost - this.postsPerPage;
+    this.currentPosts = this.posts.slice(this.indexOfFirstPost, this.indexOfLastPost);
+  }
+
+  ngOnInit() {
+    this.http.get('https://jsonplaceholder.typicode.com/posts')
+      .subscribe(data => {
+        this.posts = data;
+      });
+  }
+
+  paginate(pageNumber) {
+    this.currentPage = pageNumber;
+  }
+}
--- src/app/components/list-posts/list-posts.component.ts
+++ src/app/components/list-posts/list-posts.component.ts
@@ -1,36 +1,16 @@
-import { Component, OnInit, ViewChild, AfterViewChecked } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import {PaginationComponent} from '../pagination/pagination.component';
+import { Component, OnInit } from '@angular/core';
+
+import { PaginationComponent } from '../pagination/pagination.component';
 @Component({
   selector: 'app-list-posts',
   templateUrl: './list-posts.component.html',
   styleUrls: ['./list-posts.component.css']
 })
 export class ListPostsComponent implements OnInit {
-@ViewChild(PaginationComponent, {static: false} ) child;	
-posts: any = [];
-  currentPosts: any = [];
 
-  currentPage: number = 1;
-  postsPerPage: number = 10;
-  indexOfLastPost: number;
-  indexOfFirstPost: number;
-  
-  constructor(private http: HttpClient) { }
 
-  ngOnInit()  {
-   	this.http.get('https://jsonplaceholder.typicode.com/posts')
-   	.subscribe(data => {
-   		this.posts = data;
-   	});
-  }
+  constructor() { }
+  ngOnInit() {
 
-  ngDoCheck() {
-    this.indexOfLastPost = this.currentPage * this.postsPerPage;
-    this.indexOfFirstPost = this.indexOfLastPost - this.postsPerPage;
-    this.currentPosts = this.posts.slice(this.indexOfFirstPost, this.indexOfLastPost);
-  }
-  ngAfterViewChecked() {
-    this.currentPage = this.child.currentPage;
   }
 }
